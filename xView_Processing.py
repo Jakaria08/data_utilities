e5397fa94ad4caa112c4b6ae5e66a2a1378821c5
jakaria08 <jakaria.rabbi@live.com>
2019-12-17 17:37:36 -0700

    test image list......

--- xView_Processing.py
+++ xView_Processing.py
@@ -12,9 +12,10 @@ with open('xview_class_labels.txt') as f:
     for row in csv.reader(f):
         labels[int(row[0].split(":")[0])] = row[0].split(":")[1]
 
+coords, chips, classes = wv.get_labels('../xView_train.geojson')
+
 for image in images[:6]:
     #img = wv.get_image(image)
-    coords, chips, classes = wv.get_labels('../xView_train.geojson')
     coords = coords[chips==os.path.basename(image)]
     classes = classes[chips==os.path.basename(image)]
     print([labels[i] for i in np.unique(classes)])
