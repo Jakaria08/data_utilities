7d39c56b19dbf44d84b629054c31b4ca431bf214
jakaria08 <jakaria.rabbi@live.com>
2019-12-18 00:01:36 -0700

    create chips with labels separate empty and full patch.....

--- xView_Processing.py
+++ xView_Processing.py
@@ -32,20 +32,18 @@ for image in images[:1]:
 
     for j in range(c_img.shape[0]):
         im = Image.fromarray(c_img[j])
-        if c_cls[j].all() == 0:
+        if np.asscalar(c_cls[j]) == 0:
             im.save(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/empty_chip_train_images/",
                                     os.path.splitext(os.path.basename(image))[0]+"_"+str(j)+".png"))
             new_cls_box = np.c_[ c_cls[j], c_box[j]]
             new_cls_box = np.matrix(new_cls_box)
-            print(c_cls[j])
             np.savetxt(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/empty_chip_train_images/",
                                     os.path.splitext(os.path.basename(image))[0]+"_"+str(j)+".txt"), new_cls_box, fmt='%i')
         else:
             im.save(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/chip_train_images/",
                                     os.path.splitext(os.path.basename(image))[0]+"_"+str(j)+".png"))
-            new_cls_box = np.c_[ labels_one_sixty[c_cls[j]], c_box[j]]
+            new_cls_box = np.c_[ labels_one_sixty[np.asscalar(c_cls[j])], c_box[j]]
             new_cls_box = np.matrix(new_cls_box)
-            print(c_cls[j])
             np.savetxt(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/chip_train_images/",
                                     os.path.splitext(os.path.basename(image))[0]+"_"+str(j)+".txt"), new_cls_box, fmt='%i')
 
