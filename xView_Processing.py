81e65cb90bc8db9c68daa0ed436bebba702aa858
jakaria08 <jakaria.rabbi@live.com>
2019-12-17 19:21:59 -0700

    create chips.......

--- xView_Processing.py
+++ xView_Processing.py
@@ -4,6 +4,7 @@ import numpy as np
 import csv
 import glob
 import os
+from PIL import Image
 
 images = list(sorted(glob.glob("/home/jakaria/Super_Resolution/Datasets/xView/train_images/"+"*.tif")))
 
@@ -23,8 +24,15 @@ for image in images[:1]:
     classes_chip.append(classes[chips==os.path.basename(image)])
 
     c_img, c_box, c_cls = wv.chip_image(img=img, coords=coords_chip[i], classes=classes_chip[i],
-                            shape=(500,500))
+                            shape=(256,256))
 
     for j in range(c_img.shape[0]):
-        print(c_box[j])
+        im = Image.fromarray(c_img[j])
+        im.save(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/chip_train_images/",
+                                os.path.splitext(os.path.basename(image))[0]+i+".png"))
+        new_cls_box = np.c_[ c_cls[j], c_box[j]]
+        new_cls_box = np.matrix(new_cls_box)
+        np.savetxt(os.path.join("/home/jakaria/Super_Resolution/Datasets/xView/chip_train_images/",
+                                os.path.splitext(os.path.basename(image))[0]+i+".txt"), new_cls_box)
+
     i = i + 1
