2aa58f59aa63a9eb744adcb9efbd4663762fa48d
jakaria08 <jakaria.rabbi@live.com>
2019-12-17 17:29:50 -0700

    test image list.

--- xView_Processing.py
+++ xView_Processing.py
@@ -3,6 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import glob
+import os
 
 images = list(sorted(glob.glob("/home/jakaria/Super_Resolution/Datasets/xView/train_images/"+"*.tif")))
-print(images)
+
+labels = {}
+with open('xview_class_labels.txt') as f:
+    for row in csv.reader(f):
+        labels[int(row[0].split(":")[0])] = row[0].split(":")[1]
+
+for image in range(images[0], images[4]:
+    #img = wv.get_image(image)
+    coords, chips, classes = wv.get_labels('../xView_train.geojson')
+    coords = coords[chips==os.path.basename(image)]
+    classes = classes[chips==os.path.basename(image)]
+    print(print([labels[i] for i in np.unique(classes)]))
