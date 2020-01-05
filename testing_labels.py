6039d289f9be0c4cf181fef05258efc0fdf3519d
jakaria08 <jakaria.rabbi@live.com>
2020-01-04 21:17:47 -0700

    count labels

--- testing_labels.py
+++ testing_labels.py
@@ -24,12 +24,30 @@ def print_labels():
                 obj_class = int(values[0])
                 labels.append(obj_class)
 
-    print(list(Counter(labels).keys()))
-    print(list(Counter(labels).values()))
-    print(len(Counter(labels).values()))
-    print(Counter(labels).keys())
-    print(Counter(labels).values())
+    keys = list(Counter(labels).keys())
+    values = list(Counter(labels).values())
+    print(keys)
+    print(values)
+    print(len(values))
 
+    low_freq_labels = [keys[i] for i in range(len(values)) if values[i]<100]
+    low_freq = [values[i] for i in range(len(values)) if values[i]<100]
+    print(low_freq_labels)
+    print(low_freq)
+        #delete low freqency labels
+        '''
+        for i in range(len(annotation)):
+            annotation_path = os.path.join(root, annotation[i])
+            if i%1000 == 0:
+                print(i)
+            with open(annotation_path) as f:
+                for line in f:
+                    values = (line.split())
+                    if "\ufeff" in values[0]:
+                        values[0] = values[0][-1]
+                    obj_class = int(values[0])
+                    labels.append(obj_class)
+        '''
 def train_test_split():
     shuffle(annotation)
     for i in range(len(annotation)*0.2):
