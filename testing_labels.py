6cee092bbbb4755c71d34824e30ff37f7524106b
jakaria08 <jakaria.rabbi@live.com>
2020-01-22 15:08:12 -0700

    change labels

--- testing_labels.py
+++ testing_labels.py
@@ -5,9 +5,9 @@ from collections import Counter
 from shutil import move
 from random import shuffle
 
-root = "/home/jakaria/Super_Resolution/Datasets/xView/chip_test_images/"
+train_path = "/home/jakaria/Super_Resolution/Datasets/xView/chip_train_images/"
 test_path = "/home/jakaria/Super_Resolution/Datasets/xView/chip_test_images/"
-annotation = list(sorted(glob.glob(root+"*.txt")))
+annotation = list(sorted(glob.glob(train_path+"*.txt")))
 
 #print(len(annotation))
 #print(annotation[0])
@@ -111,5 +111,31 @@ def train_test_split():
             print(image_source_path)
             print(image_destination_path)
 
+
+def change_labels():
+    # change labels 0 to n-1;
+    for i in range(len(annotation)):
+        annotation_path = os.path.join(root, annotation[i])
+        if i%1000 == 0:
+            print(i)
+        with open(annotation_path) as f:
+            for line in f:
+                values = (line.split())
+                if "\ufeff" in values[0]:
+                    values[0] = values[0][-1]
+                obj_class = int(values[0])
+                labels.append(obj_class)
+
+    keys = list(Counter(labels).keys())
+    values = list(Counter(labels).values())
+    print(keys)
+    print(values)
+    print(len(values))
+
+    with open('labels_from_one_to_sixty.txt', 'w') as f:
+    for item in values:
+        f.write("%s\n" % item)
+
 #train_test_split()
-print_labels()
+#print_labels()
+change_labels()
